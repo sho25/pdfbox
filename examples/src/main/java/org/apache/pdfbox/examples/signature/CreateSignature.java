2b41d70b5e43ebe82ca45130920ef4fa744ff29f
John Hewson <jahewson@apache.org>
2014-03-13 19:40:41 +0000

    PDFBOX-1847: clean up qualified PDSignature names
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1577296 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9e95774f8bc002549f139b91c3dcb815ea15b087

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignature.java
@@ -772,6 +772,8 @@ name|outputDocument
 return|;
 block|}
 comment|/**      * SignatureInterface implementation.      *      * This method will be called from inside of the pdfbox and create the pkcs7 signature.      * The given InputStream contains the bytes that are given by the byte range.      *      * This method is for internal use only.<-- TODO this method should be private      *      * Use your favorite cryptographic library to implement pkcs7 signature creation.      */
+annotation|@
+name|Override
 specifier|public
 name|byte
 index|[]
