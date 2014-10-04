6d1986b189494e6cf5ee50755755cf2e164e1810
Tilman Hausherr <tilman@apache.org>
2014-10-04 19:56:32 +0000

    PDFBOX-2299: initialize member variable
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1629439 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c37e87b6cb7fe8116e073a4a865d88af498c7d63

--- preflight/src/main/java/org/apache/pdfbox/preflight/font/SimpleFontValidator.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/font/SimpleFontValidator.java
@@ -199,6 +199,12 @@ operator|.
 name|getCOSObject
 argument_list|()
 expr_stmt|;
+name|this
+operator|.
+name|font
+operator|=
+name|font
+expr_stmt|;
 block|}
 specifier|public
 name|SimpleFontValidator
@@ -230,6 +236,8 @@ name|fontDictionary
 expr_stmt|;
 block|}
 comment|/**      * Call this method to validate the font wrapped by this object. If the validation failed, the error is updated in      * the FontContainer with the right error code.      *       * Errors that are saved in the container will be added on the PreflightContext if the font is used later.      *       * @return      */
+annotation|@
+name|Override
 specifier|public
 name|void
 name|validate
