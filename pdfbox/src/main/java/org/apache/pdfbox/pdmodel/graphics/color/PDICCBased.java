1fe1bba222d490508850e78daef1662aabed51a8
Tilman Hausherr <tilman@apache.org>
2014-12-15 21:22:52 +0000

    PDFBOX-2570: refactoring to get rid of double code in PDCal* / PDLab; corrected gamma according to spec
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1645762 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3ac57d7d3ce3446a7a7c19d35e22b3155217a156

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDICCBased.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDICCBased.java
@@ -301,16 +301,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|List
 import|;
 end_import
