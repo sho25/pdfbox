52d3d608a8ecae99773a217f0b1e156fe5dfc5f5
Maruan Sahyoun <msahyoun@apache.org>
2014-12-21 09:10:25 +0000

    PDFBOX-2516 don't cache field value locally
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1647113 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: df98471286b94dbc5fefc569bf8bf0ebc8abb2d7

--- examples/src/main/java/org/apache/pdfbox/examples/fdf/SetField.java
+++ examples/src/main/java/org/apache/pdfbox/examples/fdf/SetField.java
@@ -298,12 +298,7 @@ operator|)
 operator|.
 name|setValue
 argument_list|(
-name|COSName
-operator|.
-name|getPDFName
-argument_list|(
 name|value
-argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
