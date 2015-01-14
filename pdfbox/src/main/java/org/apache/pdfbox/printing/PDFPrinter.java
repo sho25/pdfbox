683c75c4b546ba6596008159e98689f3c1bdb86b
Tilman Hausherr <tilman@apache.org>
2015-01-14 21:03:18 +0000

    PDFBOX-2576: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1651824 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 129f28f021b9b40a40bb94a215e17994d452ee88

--- pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrinter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrinter.java
@@ -97,16 +97,6 @@ name|java
 operator|.
 name|awt
 operator|.
-name|Dimension
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|awt
-operator|.
 name|Graphics
 import|;
 end_import
