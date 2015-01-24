3e06aa893351b708ab3ae2c09e793e849095306c
Tilman Hausherr <tilman@apache.org>
2015-01-24 12:51:41 +0000

    PDFBOX-2576: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1654503 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2cb836b256f47dbd5f4185481f25a377dad5b805

--- examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractEmbeddedFiles.java
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractEmbeddedFiles.java
@@ -33,16 +33,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|FileNotFoundException
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|FileOutputStream
 import|;
 end_import
