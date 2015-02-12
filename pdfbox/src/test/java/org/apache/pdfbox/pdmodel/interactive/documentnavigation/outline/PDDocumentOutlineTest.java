83994baeb1e6811f1e18e879db737f1133d9c379
Tilman Hausherr <tilman@apache.org>
2015-02-12 22:10:38 +0000

    PDFBOX-2677: restore to junit 4 style as initially received
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1659422 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 41d45ad85c48df5cf7ba336a5ef9f695fcb613ae

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/outline/PDDocumentOutlineTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/outline/PDDocumentOutlineTest.java
@@ -22,12 +22,36 @@ package|;
 end_package
 
 begin_import
-import|import
+import|import static
+name|org
+operator|.
 name|junit
 operator|.
-name|framework
+name|Assert
 operator|.
-name|TestCase
+name|assertEquals
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertFalse
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Test
 import|;
 end_import
 
@@ -39,13 +63,13 @@ begin_class
 specifier|public
 class|class
 name|PDDocumentOutlineTest
-extends|extends
-name|TestCase
 block|{
 comment|/**      * see PDF 32000-1:2008 table 152      */
+annotation|@
+name|Test
 specifier|public
 name|void
-name|testOutlinesCountShouldNotBeNegative
+name|outlinesCountShouldNotBeNegative
 parameter_list|()
 block|{
 name|PDDocumentOutline
@@ -122,9 +146,11 @@ literal|0
 argument_list|)
 expr_stmt|;
 block|}
+annotation|@
+name|Test
 specifier|public
 name|void
-name|testOutlinesCount
+name|outlinesCount
 parameter_list|()
 block|{
 name|PDDocumentOutline
