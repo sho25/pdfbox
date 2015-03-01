6b8ad8a849fe00d20adcdcd97a38e35c4c89f4ef
Tilman Hausherr <tilman@apache.org>
2015-03-01 09:24:44 +0000

    PDFBOX-2576: remove implied modifiers
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1663054 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: adfad9b8bc48a0c0055de167f1d459c786b7d0dd

--- pdfbox/src/main/java/org/apache/pdfbox/filter/DCTFilter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/DCTFilter.java
@@ -266,7 +266,6 @@ decl_stmt|;
 annotation|@
 name|Override
 specifier|public
-specifier|final
 name|DecodeResult
 name|decode
 parameter_list|(
@@ -1310,7 +1309,6 @@ block|}
 annotation|@
 name|Override
 specifier|protected
-specifier|final
 name|void
 name|encode
 parameter_list|(
