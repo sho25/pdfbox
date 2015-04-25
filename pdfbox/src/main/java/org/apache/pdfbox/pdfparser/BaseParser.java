343ff8f94c14b115d60d49bae8f2cdca0391415c
Tilman Hausherr <tilman@apache.org>
2015-04-25 09:23:33 +0000

    PDFBOX-2768: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1676002 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c9dc247084c4ea62d75dcfb111ea2eddaa651437

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/BaseParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/BaseParser.java
@@ -253,20 +253,6 @@ name|pdfbox
 operator|.
 name|cos
 operator|.
-name|COSStream
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|pdfbox
-operator|.
-name|cos
-operator|.
 name|COSString
 import|;
 end_import
@@ -964,7 +950,7 @@ name|key
 argument_list|)
 return|;
 block|}
-comment|/**      * This will parse a PDF dictionary.      *      * @return The parsed dictionary.      *      * @throws IOException IF there is an error reading the stream.      */
+comment|/**      * This will parse a PDF dictionary.      *      * @return The parsed dictionary.      *      * @throws IOException If there is an error reading the stream.      */
 specifier|protected
 name|COSDictionary
 name|parseCOSDictionary
