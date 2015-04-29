147ee6771a90244d1201341d69b4d1a0179064dc
Tilman Hausherr <tilman@apache.org>
2015-04-29 06:37:34 +0000

    PDFBOX-2576: fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1676678 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5647040d4a34650661cc887c17e4e3d42ac3fa96

--- tools/src/main/java/org/apache/pdfbox/tools/PDFToImage.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PDFToImage.java
@@ -285,7 +285,7 @@ parameter_list|()
 block|{
 comment|//static class
 block|}
-comment|/**      * Infamous main method.      *      * @param args Command line arguments, should be one and a reference to a file.      *      * @throws Exception If there is an error parsing the document.      */
+comment|/**      * Infamous main method.      *      * @param args Command line arguments, should be one and a reference to a file.      *      * @throws IOException If there is an error parsing the document.      */
 specifier|public
 specifier|static
 name|void
