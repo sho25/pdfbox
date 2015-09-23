09655b344c424c0cbde5ba5935d9ca37275e982b
Tilman Hausherr <tilman@apache.org>
2015-09-23 18:41:20 +0000

    PDFBOX-2893: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1704913 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a0fa3cce7ba8e41c7d8591254663bcca468e4432

--- pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
@@ -43,16 +43,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|FileNotFoundException
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|IOException
 import|;
 end_import
