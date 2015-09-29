90b13b338a01a651774241128f1ae43c2ec76850
Tilman Hausherr <tilman@apache.org>
2015-09-29 19:46:52 +0000

    PDFBOX-2852: remove comments about openProtection, which is no longer used
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1705921 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9461ae5eef5659b843b5a668b326be9425c1f44d

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/PublicKeyDecryptionMaterial.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/PublicKeyDecryptionMaterial.java
@@ -90,7 +90,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This class holds necessary information to decrypt a PDF document  * protected by the public key security handler.  *  * To decrypt such a document, we need:  *<ul>  *<li>a valid X509 certificate which correspond to one of the recipient of the document</li>  *<li>the private key corresponding to this certificate  *<li>the password to decrypt the private key if necessary</li>  *</ul>  *  * Objects of this class can be used with the<code>openProtection</code> method of<code>PDDocument</code>.  *  * The following example shows how to decrypt a document using a PKCS#12 certificate  * (typically files with a pfx extension).  *  *<pre>  * PDDocument doc = PDDocument.load(document_path);  * KeyStore ks = KeyStore.getInstance("PKCS12");  * ks.load(new FileInputStream(certificate_path), password.toCharArray());  * PublicKeyDecryptionMaterial dm = new PublicKeyDecryptionMaterial(ks, null, password);  * doc.openProtection(dm);  *</pre>  *  * In this code sample certificate_path contains the path to the PKCS#12 certificate.  *  * @author Benoit Guillon  *   */
+comment|/**  * This class holds necessary information to decrypt a PDF document  * protected by the public key security handler.  *  * To decrypt such a document, we need:  *<ul>  *<li>a valid X509 certificate which correspond to one of the recipient of the document</li>  *<li>the private key corresponding to this certificate  *<li>the password to decrypt the private key if necessary</li>  *</ul>  *  * @author Benoit Guillon  *   */
 end_comment
 
 begin_class
