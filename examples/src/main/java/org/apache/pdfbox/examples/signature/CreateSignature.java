32e655464fad7d7e9e0b2537990cc771652286de
Tilman Hausherr <tilman@apache.org>
2015-10-12 17:20:14 +0000

    PDFBOX-2852: remove unneeded import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1708172 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6870c83c47cb49920340ac84b05dc308c75efa3f

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignature.java
@@ -195,16 +195,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Collection
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Enumeration
 import|;
 end_import
