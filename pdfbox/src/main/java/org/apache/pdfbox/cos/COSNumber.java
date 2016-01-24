4daea4ec8839d8a6d223105ec6eae4e780e154b6
Andreas Lehmkühler <lehmi@apache.org>
2016-01-24 12:17:53 +0000

    PDFBOX-3116: check MIN/MAX values of the given number and replace them if to out of bounds
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1726482 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 961162c13ba76a8634c1e9bd6fd86133481e05a3

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
@@ -260,8 +260,6 @@ name|e
 parameter_list|)
 block|{
 comment|// might be a huge number, see PDFBOX-3116
-try|try
-block|{
 return|return
 operator|new
 name|COSFloat
@@ -269,25 +267,6 @@ argument_list|(
 name|number
 argument_list|)
 return|;
-block|}
-catch|catch
-parameter_list|(
-name|NumberFormatException
-name|e2
-parameter_list|)
-block|{
-throw|throw
-operator|new
-name|IOException
-argument_list|(
-literal|"Not a number: "
-operator|+
-name|number
-argument_list|,
-name|e2
-argument_list|)
-throw|;
-block|}
 block|}
 block|}
 else|else
