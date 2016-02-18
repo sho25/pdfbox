a2cc686bdc0dd9ad7ab45c40a7407da34c237393
Tilman Hausherr <tilman@apache.org>
2016-02-18 16:53:31 +0000

    PDFBOX-2852: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1731095 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 57a117fc7e2f3ac5e14623e092cd30fed77c83b0

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
@@ -2283,7 +2283,7 @@ name|pageNumber
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * This will import and copy the contents from another location. Currently the content stream is stored in a scratch      * file. The scratch file is associated with the document. If you are adding a page to this document from another      * document and want to copy the contents to this document's scratch file then use this method otherwise just use      * the addPage method.      *       * @param page The page to import.      * @return The page that was imported.      *       * @throws IOException If there is an error copying the page.      */
+comment|/**      * This will import and copy the contents from another location. Currently the content stream is stored in a scratch      * file. The scratch file is associated with the document. If you are adding a page to this document from another      * document and want to copy the contents to this document's scratch file then use this method otherwise just use      * the {@link #addPage} method.      *       * Unlike {@link #addPage}, this method does a deep copy. If your page has annotations, and if      * these link to pages not in the target document, then the target document might become huge.      * What you need to do is to delete page references of such annotations. See      *<a href="http://stackoverflow.com/a/35477351/535646">here</a> for how to do this.      *      * @param page The page to import.      * @return The page that was imported.      *       * @throws IOException If there is an error copying the page.      */
 specifier|public
 name|PDPage
 name|importPage
