2a6629a2e5cae6d802c858cd589e914c7a5c49b4
Tilman Hausherr <tilman@apache.org>
2016-02-18 19:27:06 +0000

    PDFBOX-2852: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1731122 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 691e607e8721aa0f0723338d7776955c80d809f3

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPageContentStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPageContentStream.java
@@ -812,7 +812,7 @@ literal|false
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Create a new PDPage content stream.      *      * @param document The document the page is part of.      * @param sourcePage The page to write the contents to.      * @param appendContent Indicates whether content will be overwritten. If false all previous      *                      content is deleted.      * @param compress Tell if the content stream should compress the page contents.      * @param resetContext Tell if the graphic context should be reseted.      * @throws IOException If there is an error writing to the page contents.      * @deprecated use {@link #PDPageContentStream(PDDocument, PDPage, PDPageContentStream.AppendMode, boolean, boolean) }      */
+comment|/**      * Create a new PDPage content stream.      *      * @param document The document the page is part of.      * @param sourcePage The page to write the contents to.      * @param appendContent Indicates whether content will be overwritten. If false all previous      *                      content is deleted.      * @param compress Tell if the content stream should compress the page contents.      * @param resetContext Tell if the graphic context should be reseted. You should use this when      * appending to an existing stream, because the existing stream may have changed graphic      * properties (e.g. scaling, rotation).      * @throws IOException If there is an error writing to the page contents.      * @deprecated use {@link #PDPageContentStream(PDDocument, PDPage, PDPageContentStream.AppendMode, boolean, boolean) }      */
 annotation|@
 name|Deprecated
 specifier|public
@@ -858,7 +858,7 @@ name|resetContext
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Create a new PDPage content stream.      *      * @param document The document the page is part of.      * @param sourcePage The page to write the contents to.      * @param appendContent Indicates whether content will be overwritten, appended or prepended.      * @param compress Tell if the content stream should compress the page contents.      * @param resetContext Tell if the graphic context should be reset. This is only relevant      *                     when the appendContent parameter is set to {@link AppendMode#APPEND}.      * @throws IOException If there is an error writing to the page contents.      */
+comment|/**      * Create a new PDPage content stream.      *      * @param document The document the page is part of.      * @param sourcePage The page to write the contents to.      * @param appendContent Indicates whether content will be overwritten, appended or prepended.      * @param compress Tell if the content stream should compress the page contents.      * @param resetContext Tell if the graphic context should be reset. This is only relevant when      * the appendContent parameter is set to {@link AppendMode#APPEND}. You should use this when      * appending to an existing stream, because the existing stream may have changed graphic      * properties (e.g. scaling, rotation).      * @throws IOException If there is an error writing to the page contents.      */
 specifier|public
 name|PDPageContentStream
 parameter_list|(
