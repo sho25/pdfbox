f0d1ca587de716a9576c81e0117c88a13ec1dd4c
Tilman Hausherr <tilman@apache.org>
2016-07-03 10:00:42 +0000

    PDFBOX-3405: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1751141 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 99229b1bb6d80cf81303edcdf5b945899cd6c5f8

--- pdfbox/src/main/java/org/apache/pdfbox/text/TextPosition.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/TextPosition.java
@@ -555,7 +555,7 @@ init|=
 operator|-
 literal|1
 decl_stmt|;
-comment|/**      * Constructor.      *      * @param pageRotation rotation of the page that the text is located in      * @param pageWidth rotation of the page that the text is located in      * @param pageHeight rotation of the page that the text is located in      * @param textMatrix text rendering matrix for start of text (in display units)      * @param endX x coordinate of the end position      * @param endY y coordinate of the end position      * @param maxHeight Maximum height of text (in display units)      * @param individualWidth The width of the given character/string. (in text units)      * @param spaceWidth The width of the space character. (in display units)      * @param unicode The string of Unicode characters to be displayed.      * @param charCodes An array of the internal PDF character codes for the glyphs in this text.      * @param font The current font for this text position.      * @param fontSize The new font size.      * @param fontSizeInPt The font size in pt units.      */
+comment|/**      * Constructor.      *      * @param pageRotation rotation of the page that the text is located in      * @param pageWidth rotation of the page that the text is located in      * @param pageHeight rotation of the page that the text is located in      * @param textMatrix text rendering matrix for start of text (in display units)      * @param endX x coordinate of the end position      * @param endY y coordinate of the end position      * @param maxHeight Maximum height of text (in display units)      * @param individualWidth The width of the given character/string. (in text units)      * @param spaceWidth The width of the space character. (in display units)      * @param unicode The string of Unicode characters to be displayed.      * @param charCodes An array of the internal PDF character codes for the glyphs in this text.      * @param font The current font for this text position.      * @param fontSize The new font size.      * @param fontSizeInPt The font size in pt units (see {@link #getFontSizeInPt()} for details).      */
 specifier|public
 name|TextPosition
 parameter_list|(
@@ -763,7 +763,7 @@ return|return
 name|charCodes
 return|;
 block|}
-comment|/**      * The matrix containing the starting text position and scaling. Despite the name, it is not the      * text matrix set by the "Tm" operator, it is really the effective text rendering matrix (which      * is dependent on the current transformation matrix, the text matrix, the font size and the      * page cropbox).      *      * @return The Matrix containing the starting text position      */
+comment|/**      * The matrix containing the starting text position and scaling. Despite the name, it is not the      * text matrix set by the "Tm" operator, it is really the effective text rendering matrix (which      * is dependent on the current transformation matrix (set by the "cm" operator), the text matrix      * (set by the "Tm" operator), the font size (set by the "Tf" operator) and the page cropbox).      *      * @return The Matrix containing the starting text position      */
 specifier|public
 name|Matrix
 name|getTextMatrix
@@ -1327,7 +1327,7 @@ return|return
 name|maxHeight
 return|;
 block|}
-comment|/**      * This will get the font size that has been set with the "Tf" operator (Set text font and      * size). When the text is rendered, it may appear bigger or smaller depending on the current      * transformation matrix and the text matrix.      *      * @return The font size.      */
+comment|/**      * This will get the font size that has been set with the "Tf" operator (Set text font and      * size). When the text is rendered, it may appear bigger or smaller depending on the current      * transformation matrix (set by the "cm" operator) and the text matrix (set by the "Tm"      * operator).      *      * @return The font size.      */
 specifier|public
 name|float
 name|getFontSize
@@ -1367,7 +1367,7 @@ return|return
 name|widthOfSpace
 return|;
 block|}
-comment|/**      * @return Returns the xScale.      */
+comment|/**      * This will get the X scaling factor. This is dependent on the current transformation matrix      * (set by the "cm" operator), the text matrix (set by the "Tm" operator) and the font size (set      * by the "Tf" operator).      *      * @return The X scaling factor.      */
 specifier|public
 name|float
 name|getXScale
@@ -1380,7 +1380,7 @@ name|getScalingFactorX
 argument_list|()
 return|;
 block|}
-comment|/**      * @return Returns the yScale.      */
+comment|/**      * This will get the Y scaling factor. This is dependent on the current transformation matrix      * (set by the "cm" operator), the text matrix (set by the "Tm" operator) and the font size (set      * by the "Tf" operator).      *      * @return The X scaling factor.      */
 specifier|public
 name|float
 name|getYScale
