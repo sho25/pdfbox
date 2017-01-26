1fa9249e49af215a7ba622e0192db142870de0d9
Tilman Hausherr <tilman@apache.org>
2017-01-26 16:01:31 +0000

    PDFBOX-3663: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1780415 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d5e52d4a71c9654c765bdd2c4d4769f11de23fc5

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroForm.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroForm.java
@@ -1468,7 +1468,7 @@ expr_stmt|;
 block|}
 block|}
 block|}
-comment|/**      * This will return all of the documents root fields.      *       * A field might have children that are fields (non-terminal field) or does not      * have children which are fields (terminal fields).      *       * The fields within an AcroForm are organized in a tree structure. The documents root fields       * might either be terminal fields, non-terminal fields or a mixture of both. Non-terminal fields      * mark branches which contents can be retrieved using {@link PDNonTerminalField#getChildren()}.      *       * @return A list of the documents root fields.      *       */
+comment|/**      * This will return all of the documents root fields.      *       * A field might have children that are fields (non-terminal field) or does not      * have children which are fields (terminal fields).      *       * The fields within an AcroForm are organized in a tree structure. The documents root fields       * might either be terminal fields, non-terminal fields or a mixture of both. Non-terminal fields      * mark branches which contents can be retrieved using {@link PDNonTerminalField#getChildren()}.      *       * @return A list of the documents root fields, never null. If there are no fields then this      * method returns an empty list.      */
 specifier|public
 name|List
 argument_list|<
