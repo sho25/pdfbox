1d8c88fffba3df82a295bf2ee4f6d8a06dacea95
Tilman Hausherr <tilman@apache.org>
2017-02-02 16:29:44 +0000

    PDFBOX-2852: remove unused
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1781426 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 919f13cc4302e22397e9bcf481e8b8c463c681a9

--- pdfbox/src/test/java/org/apache/pdfbox/cos/TestCOSUpdateInfo.java
+++ pdfbox/src/test/java/org/apache/pdfbox/cos/TestCOSUpdateInfo.java
@@ -40,28 +40,6 @@ import|;
 end_import
 
 begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|fail
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
-import|;
-end_import
-
-begin_import
 import|import
 name|org
 operator|.
