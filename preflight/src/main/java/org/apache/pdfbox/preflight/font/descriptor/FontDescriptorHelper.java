f133f57f8ab7467b0836cdfa6ef8288f89e3a315
Tilman Hausherr <tilman@apache.org>
2017-06-07 16:18:46 +0000

    PDFBOX-2852: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1797966 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6a52c49b8320e925830fd802997f6bc1ab6476bf

--- preflight/src/main/java/org/apache/pdfbox/preflight/font/descriptor/FontDescriptorHelper.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/font/descriptor/FontDescriptorHelper.java
@@ -265,16 +265,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|ByteArrayOutputStream
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|IOException
 import|;
 end_import
