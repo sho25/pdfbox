78d02b695e1f741c0a8961dc2930cc6e2f83333b
Maruan Sahyoun <msahyoun@apache.org>
2018-03-08 09:13:08 +0000

    PDFBOX-4071: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1826191 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7b0c5389e0feb7d7c71657d11141851a25f0f04d

--- debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
@@ -117,16 +117,6 @@ begin_import
 import|import
 name|java
 operator|.
-name|io
-operator|.
-name|UnsupportedEncodingException
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
 name|util
 operator|.
 name|List
