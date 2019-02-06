645a1f1ebc44122e1db175a919019437d434d780
Tilman Hausherr <tilman@apache.org>
2019-02-06 18:19:51 +0000

    PDFBOX-4071: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1853090 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b8e0cc857e2f198947a058907ff1a9cdb937ec61

--- pdfbox/src/test/java/org/apache/pdfbox/multipdf/MergeAcroFormsTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/multipdf/MergeAcroFormsTest.java
@@ -93,16 +93,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|net
-operator|.
-name|URL
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
