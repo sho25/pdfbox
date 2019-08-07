30158aabf78fb334b9d01999e4855a013208a478
Tilman Hausherr <tilman@apache.org>
2019-08-07 17:13:22 +0000

    PDFBOX-4625: remove public when parameter class is package-private
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1864646 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7a35c1a2414fd092aa627e1dc37fe01960997e02

--- fontbox/src/main/java/org/apache/fontbox/ttf/IndexToLocationTable.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/IndexToLocationTable.java
@@ -81,7 +81,6 @@ block|}
 comment|/**      * This will read the required data from the stream.      *       * @param ttf The font that is being read.      * @param data The stream to read the data from.      * @throws IOException If there is an error reading the data.      */
 annotation|@
 name|Override
-specifier|public
 name|void
 name|read
 parameter_list|(
