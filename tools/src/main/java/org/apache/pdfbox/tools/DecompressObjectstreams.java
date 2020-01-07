b52dc997fafbf627d93d53658c90f722213dab88
Tilman Hausherr <tilman@apache.org>
2020-01-07 20:22:58 +0000

    PDFBOX-4071: catch actual exception
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1872465 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: feaacd376b636b043a7c9cb577a0b04b340cc8ea

--- tools/src/main/java/org/apache/pdfbox/tools/DecompressObjectstreams.java
+++ tools/src/main/java/org/apache/pdfbox/tools/DecompressObjectstreams.java
@@ -27,6 +27,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -366,7 +376,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|Exception
+name|IOException
 name|e
 parameter_list|)
 block|{
