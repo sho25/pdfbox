7a6e5a8b4092a80277523bb1758e5ba323beef2d
Tilman Hausherr <tilman@apache.org>
2020-02-28 09:12:21 +0000

    PDFBOX-4788: add disabled test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1874609 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2582c72afa7ce81353a17adef92cf0e88d65a435

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroFormFlattenTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroFormFlattenTest.java
@@ -733,6 +733,33 @@ name|targetFileName
 argument_list|)
 expr_stmt|;
 block|}
+comment|/**      * PDFBOX-4788: non-widget annotations are not to be removed on a page that has no widget      * annotations.      */
+comment|// @Test
+specifier|public
+name|void
+name|testFlattenPDFBox4788
+parameter_list|()
+throws|throws
+name|IOException
+block|{
+name|String
+name|sourceUrl
+init|=
+literal|"https://issues.apache.org/jira/secure/attachment/12994791/flatten.pdf"
+decl_stmt|;
+name|String
+name|targetFileName
+init|=
+literal|"PDFBOX-4788.pdf"
+decl_stmt|;
+name|flattenAndCompare
+argument_list|(
+name|sourceUrl
+argument_list|,
+name|targetFileName
+argument_list|)
+expr_stmt|;
+block|}
 comment|/*      * Flatten and compare with generated image samples.      */
 specifier|private
 specifier|static
